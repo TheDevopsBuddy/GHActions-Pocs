52608facda9bb9a8b77aef94d1de37d7e6da0d9d
Mangesh Arun Suryawanshi <mangeshsu@cybage.com>
2025-03-11 14:14:45 +0530

    script runner

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the output file
+FILE="date_time.txt"
+
+# Get the current date and time in the desired format
+CURRENT_DATE_TIME=$(date +"%d-%m-%Y : %H:%M")
+
+# Write the date and time to the file
+echo $CURRENT_DATE_TIME > $FILE
+
+# Output a confirmation message
+echo "The current date and time have been written to $FILE"
